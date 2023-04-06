a98792587f841cd5b17ebaef319e16288426722a
isadorateles <isadoratelesdecastro@gmail.com>
2023-04-06 12:05:53 +0200

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -4,6 +4,9 @@ let px = 50; // Position x and y
 let py = 50;
 let vx = 0.0; // Velocity x and y
 let vy = 0.0;
+let x = 0.0;
+let y = 0.0;
+let mobile = false;
 const updateRate = 1/60; // Sensor refresh rate
 
 let rotation_degrees = 0;
@@ -19,18 +22,29 @@ ws.onmessage = function (event)
 {
     let data = JSON.parse(event.data);
     let stringifiedData = data.toString();
-    if(stringifiedData === 'ping') {
+    
+    if(stringifiedData === 'ping') 
+    {
         ws.send('pong');
         console.log('got ping, sent pong');
         return;
     }
 
-    if (data.type === 'sensorData') {
+    if (data.type === 'sensorData') 
+    {
         let myId = data.id;
         let valAlpha = data.a;
         let valBeta = data.b;
         let valueGamma = data.g; 
         console.log('Got : ', myId, valAlpha, valBeta, valueGamma);
+    }
+
+    if (data.type === 'mouseData') 
+    {
+        let myId = data.id;
+        let valX = data.x;
+        let valY = data.y;
+        console.log('Got : ', myId, valX, valY);
     }
 };
 
@@ -46,7 +60,9 @@ ws.onclose = function () {
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
 // If the device is a mobile phone, listen for sensor data
-if (isMobile) {
+if (isMobile) 
+{
+    mobile == true;
     // Add a listener to get smartphone orientation 
     // in the alpha-beta-gamma axes (units in degrees)
     window.addEventListener('deviceorientation', (event) => {
@@ -58,33 +74,57 @@ if (isMobile) {
         ws.send(JSON.stringify({'type': 'sensorData', 'id' : ws.id , 'a': rotation_degrees, 'b': frontToBack_degrees, 'g': leftToRight_degrees}));
     });
 }
+else
+{
+    mobile == false;
 
-function setup() {
+    document.addEventListener('mousedown', function(event) 
+    {
+        // Get the x and y coordinates of the mouse click
+        x = event.clientX;
+        y = event.clientY;
+        
+        //console.log("Mouse clicked at position: x=" + x + ", y=" + y);
+        ws.send(JSON.stringify({'type': 'mouseData', 'id' : ws.id , 'x': x, 'y': y}));
+      });
+}
+
+function setup() 
+{
     createCanvas(400, 400);
     ellipseMode(CENTER);
 }
   
-function draw() {
+function draw() 
+{
     background(220);
 
-    // Update velocity according to how tilted the phone is
-    // Since phones are narrower than they are long, double the increase to the x velocity
-    vx += leftToRight_degrees * updateRate * 2; 
-    vy += frontToBack_degrees * updateRate;
+    if(mobile)
+    {
+        // Update velocity according to how tilted the phone is
+        // Since phones are narrower than they are long, double the increase to the x velocity
+        vx += leftToRight_degrees * updateRate * 2; 
+        vy += frontToBack_degrees * updateRate;
 
-    // Update position and clip it to bounds
-    px += vx * 0.5;
-    if (px > width || px < 0) { 
-        px = Math.max(0, Math.min(398, px)); // Clip px between 0-398
-        vx = 0;
+        // Update position and clip it to bounds
+        px += vx * 0.5;
+        if (px > width || px < 0) { 
+            px = Math.max(0, Math.min(398, px)); // Clip px between 0-398
+            vx = 0;
+        }
+
+        py += vy * 0.5;
+        if (py > height || py < 0) {
+            py = Math.max(0, Math.min(398, py)); // Clip py between 0-398
+            vy = 0;
+        }
+
+        ellipse(px, py, 50, 50);
     }
 
-    py += vy * 0.5;
-    if (py > height || py < 0) {
-        py = Math.max(0, Math.min(398, py)); // Clip py between 0-398
-        vy = 0;
+    else 
+    {
+        ellipse(x, y, 50, 50);
     }
-
-
-    ellipse(px, py, 50, 50);
+    
 }
