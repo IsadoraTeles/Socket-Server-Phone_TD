e70399024629624775dc20f0e41412230f640594
isadorateles <isadoratelesdecastro@gmail.com>
2023-11-08 09:18:59 +0100

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -162,7 +162,7 @@ if (isMobile)
     function handleOrientation(event) 
     {
         let smoothing_factor = 0; // Adjust this between 0 (no smoothing) and 1 (maximum smoothing)
-        let scale_factor = 1; // Scale factor for adjusting sensor data range to canvas range
+        let scale_factor = 2; // Scale factor for adjusting sensor data range to canvas range
         let damping_factor = 1; // Damping factor to gradually reduce velocity
 
         let new_vx = 0, new_vy = 0;
